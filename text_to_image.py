9d2d3ccdd003341fdf21773ee5569d7de7046a4c
Marcus Kellerman <sharkus@gmail.com>
2020-12-28 12:40:06 -0800

    Separated image and video creation into separate scripts

--- text_to_image.py
+++ text_to_image.py
@@ -2,55 +2,6 @@ import argparse
 from PIL import Image, ImageDraw, ImageFont
 import os
 import math
-from moviepy.editor import *
-from moviepy.video.tools.drawing import color_gradient
-
-def create_video_from_text(text, args):
-    moviesize = tuple(map(int, args.size.split('x')))
-    w, h = moviesize
-
-    # Add blanks
-    # text = 5*"\n" + text + 5*"\n"
-    if args.font_size == "":
-        font_size = 100
-    else:
-        font_size = int(args.font_size)
-
-    # CREATE THE TEXT IMAGE
-    clip_txt = TextClip(text.strip(), bg_color=args.background_color, color=args.text_color,
-        align='Center', fontsize=font_size, font=args.font, method='label')
-    
-    # SCROLL THE TEXT IMAGE BY CROPPING A MOVING AREA
-
-    pause_time = 2
-    if args.text_speed == "":
-        duration = int(args.duration)
-        txt_speed = (clip_txt.size[1] - h) / (duration - (2 * pause_time))
-    else:
-        txt_speed = int(args.text_speed)
-        duration = 2 * pause_time + ((clip_txt.size[1] - h) / txt_speed)
-
-    def fl(gf, t):
-        if t < pause_time:
-            # Pause at start
-            t = 0
-        elif t > duration - pause_time:
-            # Pause at end
-            t = duration - (2 * pause_time)
-        else:
-            # Scroll in between pauses at start and end
-            t = t - pause_time
-
-        return gf(t)[int(txt_speed*t):int(txt_speed*t)+h,:]
-
-    moving_txt = clip_txt.fl(fl, apply_to=['mask'])
-
-    final = CompositeVideoClip([
-            moving_txt.set_pos(('center','bottom'))],
-            size = moviesize)
-
-    # WRITE TO A FILE
-    final.set_duration(duration).write_videofile(args.save_name, fps=int(args.fps)) #, preset="ultrafast")
 
 def create_image_from_text(text, args):
     lines = text.strip().splitlines()
@@ -142,26 +93,24 @@ def create_image_from_text(text, args):
 
     img.save(args.save_name)
 
+def image_file(fname):
+    file_type = os.path.splitext(fname)[1] 
+    if file_type not in ['.png', '.jpg']:
+        raise argparse.ArgumentTypeError
+    return fname
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("text_file", help="Text file to convert to image or video")
-    parser.add_argument("save_name", help="File path of output graphic (.png or .jpg) or video file (.mp4 or .avi)")
+    parser.add_argument("save_name", type=image_file, help="File path of output graphic (.png or .jpg)")
     parser.add_argument("font", help="Full path to TrueType or OpenType font file")
-    parser.add_argument("--size", help="Size of image or video to generate in format of WxH (e.g. '1776x445')", default="1776x445")
+    parser.add_argument("--size", help="Size of image to generate in format of WxH (e.g. '1776x445')", default="1776x445")
     parser.add_argument("--background_color", help="Background color", default="black")
     parser.add_argument("--text_color", help="Color of text", default="grey")
     parser.add_argument("--font_size", help="Name or path to font (default will maximize image)", default="")
-    parser.add_argument("--max_lines", help="(image only) Maximum number of lines", default="8")
-    parser.add_argument("--duration", help="(video only) Length of time for video clip", default="10")
-    parser.add_argument("--text_speed", help="(video only) Speed of text scrolling (overrides duration) -- 120 is reasonable", default="")
-    parser.add_argument("--fps", help="(video only) FPS of generated video file", default="12")
+    parser.add_argument("--max_lines", help="Maximum number of lines", default="8")
     args = parser.parse_args()
 
-    if args.text_file != "":
-        with open(args.text_file, 'r') as f:
-            text = f.read()
-            file_type = os.path.splitext(args.save_name)[1] 
-            if file_type in ['.png', '.jpg']:
-                create_image_from_text(text, args)
-            elif file_type in ['.mp4', '.avi']:
-                create_video_from_text(text, args)
+    with open(args.text_file, 'r') as f:
+        text = f.read()
+        create_image_from_text(text, args)
---
+++ text_to_video.py
@@ -0,0 +1,75 @@
+import argparse
+import os
+from moviepy.editor import *
+from moviepy.video.tools.drawing import color_gradient
+
+def create_video_from_text(text, args):
+    moviesize = tuple(map(int, args.size.split('x')))
+    w, h = moviesize
+
+    # Add blanks
+    # text = 5*"\n" + text + 5*"\n"
+    if args.font_size == "":
+        font_size = 100
+    else:
+        font_size = int(args.font_size)
+
+    # CREATE THE TEXT IMAGE
+    clip_txt = TextClip(text.strip(), bg_color=args.background_color, color=args.text_color,
+        align='Center', fontsize=font_size, font=args.font, method='label')
+    
+    # SCROLL THE TEXT IMAGE BY CROPPING A MOVING AREA
+
+    pause_time = 2
+    if args.text_speed == "":
+        duration = int(args.duration)
+        txt_speed = (clip_txt.size[1] - h) / (duration - (2 * pause_time))
+    else:
+        txt_speed = int(args.text_speed)
+        duration = 2 * pause_time + ((clip_txt.size[1] - h) / txt_speed)
+
+    def fl(gf, t):
+        if t < pause_time:
+            # Pause at start
+            t = 0
+        elif t > duration - pause_time:
+            # Pause at end
+            t = duration - (2 * pause_time)
+        else:
+            # Scroll in between pauses at start and end
+            t = t - pause_time
+
+        return gf(t)[int(txt_speed*t):int(txt_speed*t)+h,:]
+
+    moving_txt = clip_txt.fl(fl, apply_to=['mask'])
+
+    final = CompositeVideoClip([
+            moving_txt.set_pos(('center','bottom'))],
+            size = moviesize)
+
+    # WRITE TO A FILE
+    final.set_duration(duration).write_videofile(args.save_name, fps=int(args.fps)) #, preset="ultrafast")
+
+def video_file(fname):
+    file_type = os.path.splitext(fname)[1] 
+    if file_type not in ['.mp4', '.avi']:
+        raise argparse.ArgumentTypeError
+    return fname
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("text_file", help="Text file to convert to image or video")
+    parser.add_argument("save_name", type=video_file, help="File path of output video file (.mp4 or .avi)")
+    parser.add_argument("font", help="Full path to TrueType or OpenType font file")
+    parser.add_argument("--size", help="Size of image or video to generate in format of WxH (e.g. '1776x445')", default="1776x445")
+    parser.add_argument("--background_color", help="Background color", default="black")
+    parser.add_argument("--text_color", help="Color of text", default="grey")
+    parser.add_argument("--font_size", help="Name or path to font (default will maximize image)", default="")
+    parser.add_argument("--duration", help="Length of time for video clip", default="10")
+    parser.add_argument("--text_speed", help="Speed of text scrolling (overrides duration) -- 120 is reasonable", default="")
+    parser.add_argument("--fps", help="FPS of generated video file", default="12")
+    args = parser.parse_args()
+
+    with open(args.text_file, 'r') as f:
+        text = f.read()
+        create_video_from_text(text, args)
