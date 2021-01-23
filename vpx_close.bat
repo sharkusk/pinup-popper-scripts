e4b99c6d7e37253dbfb7d7474ca19f0b00b0437b
Marcus Kellerman <sharkus@gmail.com>
2021-01-22 21:28:55 -0800

    Launch batch files from popper instead of copying long scripts

--- vpx_close.bat
+++ vpx_close.bat
@@ -1,17 +1,54 @@
-ECHO Closing VPX>> "[STARTDIR]\scripts\logs\debug.log"
-START "" "[STARTDIR]LAUNCH\PUPCLOSER.EXE" WINTIT "Visual Pinball" 10 1
+REM Call this batch file from Popper's Visual Pinball X Close Script.  It should be the last line.
+REM CALL [STARTDIR]Scripts\vpx_launch.bat "[DIREMU]" "[DIRGAME]" "[DIRROM]" "[GAMEFULLNAME]" "[GAMENAME]" "[GAMEEXT]" "[STARTDIR]" "[CUSTOM1]" "[CUSTOM2]" "[CUSTOM3]" "[ALTEXE]" "[ALTMODE]" "[MEDIADIR]" "[?ROM?]" "[?GameType?]"
 
-TIMEOUT /T 1 /NOBREAK
+REM Use shift since we have more than 9 parameters
+REM Remove quotes from variables
+SET DIREMU=%~1
+SHIFT
+SET DIRGAME=%~1
+SHIFT
+SET DIRROM=%~1
+SHIFT
+SET GAMEFULLNAME=%~1
+SHIFT
+SET GAMENAME=%~1
+SHIFT
+SET GAMEEXT=%~1
+SHIFT
+SET STARTDIR=%~1
+SHIFT
+SET CUSTOM1=%~1
+SHIFT
+SET CUSTOM2=%~1
+SHIFT
+SET CUSTOM3=%~1
+SHIFT
+SET ALTEXE=%~1
+SHIFT
+SET ALTMODE=%~1
+SHIFT
+SET MEDIADIR=%~1
+SHIFT
+REM ?game_field? entries...
+SET ROM=%~1
+SHIFT
+SET GameType=%~1
+SHIFT
 
-IF NOT "[?ROM?]"=="" (
-    ECHO "[STARTDIR]Scripts\hiscore.bat" [?ROM?] "[GAMENAME]" "[?GameType?]">> "[STARTDIR]\scripts\logs\debug.log"
+ECHO VPX close script start>> "%STARTDIR%\scripts\logs\debug.log"
+
+IF NOT "%ROM%"=="" (
+    ECHO Starting high score generation...>> "%STARTDIR%\scripts\logs\debug.log"
+    ECHO "%STARTDIR%Scripts\hiscore.bat" %ROM% "%GAMENAME%" "%GameType%">> "%STARTDIR%\scripts\logs\debug.log"
 
     REM Generate HiScore media file
-    CALL "[STARTDIR]Scripts\hiscore.bat" [?ROM?] "[GAMENAME]" "[?GameType?]"
-    ECHO Generated high score>> "[STARTDIR]\scripts\logs\debug.log"
+    CALL "%STARTDIR%Scripts\hiscore.bat" %ROM% "%GAMENAME%" "%GameType%"
+    ECHO Completed high score generation>> "%STARTDIR%\scripts\logs\debug.log"
 )
 
-CALL "[STARTDIR]restore_settings.bat"
-REM DEL "[STARTDIR]restore_settings.bat"
+ECHO Restoring settings...>> "%STARTDIR%\scripts\logs\debug.log"
+CALL "%STARTDIR%restore_settings.bat"
+REM DEL "%STARTDIR%restore_settings.bat"
 
-ECHO Restored settings... Done.>> "[STARTDIR]\scripts\logs\debug.log"
+ECHO Settings have been restored.>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO VPX close script end>> "%STARTDIR%\scripts\logs\debug.log"
--- vpx_launch.bat
+++ vpx_launch.bat
@@ -1,71 +1,120 @@
+REM Call this batch file from Popper's Visual Pinball X Launch Script.  It should be the first line.
+REM CALL [STARTDIR]Scripts\vpx_launch.bat "[DIREMU]" "[DIRGAME]" "[DIRROM]" "[GAMEFULLNAME]" "[GAMENAME]" "[GAMEEXT]" "[STARTDIR]" "[CUSTOM1]" "[CUSTOM2]" "[CUSTOM3]" "[ALTEXE]" "[ALTMODE]" "[MEDIADIR]" "[?ROM?]" "[?GameType?]"
+
 REM VPX Pinup Popper Launch Script (by Sharkus)
-REM This entire file should be copied to the "Launch Script" found in Popper Setup->Emulators->Launch Setup (Visual Pinball X)
 
 REM As this launch script executes, it creates "restore_settings.bat" that will revert all changes.
 REM This keeps all of the code co-located and prevents accidents (i.e. bugs).
 REM "restore_settings.bat" should be called from the close script.
 
 REM Altmodes supported:
-REM backglass - do not run a pup-pack and use a backglass named [tablename].directb2s.BG
+REM backglass - do not run a pup-pack and use a backglass named [tablename].BG
 REM origsound - use the original rom sound with a forced backglass
 REM altsound - use the altsound with a forced backglass
 REM pinsound - use the pinsound with a forced backglass
 
-ECHO VPX Launch Starting... ROM: [?ROM?]> "[STARTDIR]\scripts\logs\debug.log"
+REM Use shift since we have more than 9 parameters
+REM Remove quotes from variables
+SET DIREMU=%~1
+SHIFT
+SET DIRGAME=%~1
+SHIFT
+SET DIRROM=%~1
+SHIFT
+SET GAMEFULLNAME=%~1
+SHIFT
+SET GAMENAME=%~1
+SHIFT
+SET GAMEEXT=%~1
+SHIFT
+SET STARTDIR=%~1
+SHIFT
+SET CUSTOM1=%~1
+SHIFT
+SET CUSTOM2=%~1
+SHIFT
+SET CUSTOM3=%~1
+SHIFT
+SET ALTEXE=%~1
+SHIFT
+SET ALTMODE=%~1
+SHIFT
+SET MEDIADIR=%~1
+SHIFT
+REM ?game_field? entries...
+SET ROM=%~1
+SHIFT
+SET GameType=%~1
+SHIFT
+
+ECHO VPX launch script start> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... DIREMU: %DIREMU%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... DIRGAME: %DIRGAME%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... DIRROM: %DIRROM%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... GAMEFULLNAME: %GAMEFULLNAME%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... GAMENAME: %GAMENAME%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... GAMEEXT: %GAMEEXT%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... STARTDIR: %STARTDIR%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... CUSTOM1: %CUSTOM1%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... CUSTOM2: %CUSTOM2%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... CUSTOM3: %CUSTOM3%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... ALTEXE: %ALTEXE%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... ALTMODE: %ALTMODE%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... MEDIADIR: %MEDIADIR%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... ROM: %ROM%>> "%STARTDIR%\scripts\logs\debug.log"
+ECHO ... GameType: %GameType%>> "%STARTDIR%\scripts\logs\debug.log"
 
 REM Initialize a file that is going to restore any settings we change when we close
-ECHO REM Restore settings for [GAMENAME]> "[STARTDIR]restore_settings.bat" 
+ECHO REM Restore settings for %GAMENAME%> "%STARTDIR%restore_settings.bat" 
 
 SET use_backglass=0
 
-IF "[ALTMODE]"=="backglass" (
+IF "%ALTMODE%"=="backglass" (
     SET use_backglass=1
 )
 
 REM Check if we are dealing with a PinMAME ROM
-REG QUERY "HKCU\Software\Freeware\Visual PinMame\[?ROM?]" /v "sound_mode"
+REG QUERY "HKCU\Software\Freeware\Visual PinMame\%ROM%" /v "sound_mode"
 IF %errorlevel%==0 (
+    ECHO Detected PinMAME ROM>> "%STARTDIR%\scripts\logs\debug.log"
     SET PinMAME=1
 ) else (
+    ECHO Non PinMAME Table found>> "%STARTDIR%\scripts\logs\debug.log"
     SET PinMAME=0
 )
 
 IF %PinMAME%==1 (
-    CALL "[STARTDIR]scripts\vpx_regset.bat" [?ROM?] "[ALTMODE]" "[STARTDIR]restore_settings.bat"
+    ECHO Setting PinMAME register settings...>> "%STARTDIR%\scripts\logs\debug.log"
+    CALL "%STARTDIR%scripts\vpx_regset.bat" %ROM% "%ALTMODE%" "%STARTDIR%restore_settings.bat"
+    ECHO Register settings complete>> "%STARTDIR%\scripts\logs\debug.log"
 )
 
 IF %use_backglass%==1 (
-    IF EXIST "[STARTDIR]PUPVideos\[?ROM?]\" (
+    ECHO Using backglass...>> "%STARTDIR%\scripts\logs\debug.log"
+    IF EXIST "%STARTDIR%PUPVideos\%ROM%\" (
+        ECHO Telling pinup to hide next...>> "%STARTDIR%\scripts\logs\debug.log"
         REM Tell PinUp Player to not use Pup pack this time around
-        ECHO 1>> "[STARTDIR]PUPVideos\[?ROM?]\PUPHideNext.txt"
+        ECHO 1>> "%STARTDIR%PUPVideos\%ROM%\PUPHideNext.txt"
     )
 
     REM If we have a backglass available, copy it and delete on exit to restore law and order
-    IF EXIST "[DIRGAME]\[GAMENAME].directb2s.BG" (
-        COPY /Y "[DIRGAME]\[GAMENAME].directb2s.BG" "[DIRGAME]\[GAMENAME].directb2s"
+    IF EXIST "%DIRGAME%\%GAMENAME%.BG" (
+        ECHO Copying "%DIRGAME%\%GAMENAME%.BG" to "%DIRGAME%\%GAMENAME%.directb2s">> "%STARTDIR%\scripts\logs\debug.log"
+        COPY /Y "%DIRGAME%\%GAMENAME%.BG" "%DIRGAME%\%GAMENAME%.directb2s"
 
         REM -----------  CLEANUP ON CLOSE -----------
-        ECHO DEL "[DIRGAME]\[GAMENAME].directb2s">> "[STARTDIR]restore_settings.bat"
+        ECHO DEL "%DIRGAME%\%GAMENAME%.directb2s">> "%STARTDIR%restore_settings.bat"
     )
 
-    IF EXIST "[DIRROM]\[?ROM?].zip.BG" (
-        IF NOT EXIST "[DIRROM]\[?ROM?].zip.pinup" (
-            COPY /Y "[DIRROM]\[?ROM?].zip" "[DIRROM]\[?ROM?].zip.pinup"
+    IF EXIST "%DIRROM%\%ROM%.zip.BG" (
+        IF NOT EXIST "%DIRROM%\%ROM%.zip.pinup" (
+            COPY /Y "%DIRROM%\%ROM%.zip" "%DIRROM%\%ROM%.zip.pinup"
         )
-        COPY /Y "[DIRROM]\[?ROM?].zip.BG" "[DIRROM]\[?ROM?].zip"
+        ECHO Copying "%DIRROM%\%ROM%.zip.BG" to "%DIRROM%\%ROM%.zip">> "%STARTDIR%\scripts\logs\debug.log"
+        COPY /Y "%DIRROM%\%ROM%.zip.BG" "%DIRROM%\%ROM%.zip"
 
         REM -----------  CLEANUP ON CLOSE -----------
-        ECHO COPY /Y "[DIRROM]\[?ROM?].zip.pinup" "[DIRROM]\[?ROM?].zip">> "[STARTDIR]restore_settings.bat"
+        ECHO COPY /Y "%DIRROM%\%ROM%.zip.pinup" "%DIRROM%\%ROM%.zip">> "%STARTDIR%restore_settings.bat"
     )
 )
-
-TIMEOUT /T 1 /NOBREAK
-
-START "" "[STARTDIR]Launch\VPXSTARTER.exe" 30 10 60 “Visual Pinball Player” 2
-CD /d "[DIREMU]"
-
-IF "[RECMODE]"=="1" (
-    START /min "" vpinballx.exe "[DIREMU]" -DisableTrueFullscreen -minimized -play "[GAMEFULLNAME]"
-) else (
-    START /min "" vpinballx.exe "[DIREMU]" -minimized -play "[GAMEFULLNAME]"
-)
+ECHO VPX launch script complete>> "%STARTDIR%\scripts\logs\debug.log"
